ab1bc0d240871a76326973390fa7c6efeea8a7dc
mialskywalker <mialskywalker@gmail.com>
2022-10-22 10:30:22 +0200

    part 3

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ kp1, des1 = orb.detectAndCompute(imgTarget, None)
 
 while True:
     successful, imgWebcam = cap.read()
+    imgAug = imgWebcam.copy()
     kp2, des2 = orb.detectAndCompute(imgWebcam, None)
     # imgWebcam = cv2.drawKeypoints(imgWebcam, kp2, None)
 
@@ -36,6 +37,31 @@ while True:
     print(len(good))
     imgFeatures = cv2.drawMatches(imgTarget, kp1, imgWebcam, kp2, good, None, flags=2)
 
+    # Homography
+    if len(good) > 20:
+        srcPts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+        dstPts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+        matrix, mask = cv2.findHomography(srcPts, dstPts, cv2.RANSAC, 5)
+        print(matrix)
+
+        # Frame of the picture/video calculation
+        pts = np.float32([[0, 0], [0, hT], [wT, hT], [wT, 0]]).reshape(-1, 1, 2)
+        dst = cv2.perspectiveTransform(pts, matrix)
+        img2 = cv2.polylines(imgWebcam, [np.int32(dst)], True, (255, 0, 255), 3)
+
+        imgWarp = cv2.warpPerspective(imgVideo, matrix, (imgWebcam.shape[1], imgWebcam.shape[0]))
+
+        # Overlaying
+        maskNew = np.zeros((imgWebcam.shape[0], imgWebcam.shape[1]), np.uint8)
+        cv2.fillPoly(maskNew, [np.int32(dst)], (255, 255, 255))
+        maskInv = cv2.bitwise_not(maskNew)
+        imgAug = cv2.bitwise_and(imgAug, imgAug, mask=maskInv)
+        imgAug = cv2.bitwise_or(imgWarp, imgAug)
+
+    cv2.imshow('Aug', imgAug)
+    cv2.imshow('imgWarp', imgWarp)
+    cv2.imshow('img2', img2)
     cv2.imshow('ImgFeatures', imgFeatures)
     cv2.imshow('ImgTarget', imgTarget)
     cv2.imshow('myVideo', imgVideo)
